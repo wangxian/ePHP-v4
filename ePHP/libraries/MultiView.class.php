b1c1d6197306b277a313922d86a1264f194a2e56
wangxian <xian366@126.com>
2013-10-30 10:39:55 +0800

    修复 MultiView bug

--- ePHP/libraries/MultiView.class.php
+++ ePHP/libraries/MultiView.class.php
@@ -16,12 +16,18 @@
 class MultiView extends view
 {
 	/**
-	 * 引用视图 或 视图片段，多视图支持
-	 * @param type $file 变量名
-	 * @param type $layout_block 是否渲染布局模版
+	 * 引用视图 或 视图片段
+	 * @param string $file 变量名
+	 * @param string $layout_block 是否渲染布局模版
+	 * @param boolean $layout_block 使用布局模版否
+	 * @param boolean $return 返回模版内容 or 直接输出
 	 */
-	public function _include($file, $layout_block=false)
+	public function _include($file, $__vars=null, $layout_block=false, $return=false)
     {
+        if( is_array($this->vars) ) extract($this->vars);
+		if( is_array($__vars) ) extract($__vars);
+
+		/* support MultiView */
 		$file = $this->__filename($file);
 		$tpl = C('tpl_switch');
 
@@ -36,22 +42,29 @@ class MultiView extends view
 		}
 
 		$file=$tpl.'/'.$file;
+		/* support MultiView */
 
-
-
-        if( is_array($this->vars) ) extract($this->vars);
 		if($layout_block)
 		{
+			$tpl = C('tpl_switch');
+
 			ob_start();
 			include APP_PATH . '/views/'. $file;
 			$content = ob_get_clean();
+			//echo $content;print_r($this->_layout);exit;
+
 			if($this->_layout)
 			{
-				$this->_layout = array_reverse($this->_layout);//反转数组
-				foreach ($this->_layout as $k=>$v)
-					$content = str_replace ("<!--{layout_block_{$k}}-->", $v,$content);
+				foreach ($this->_layout as $k=>$v) $content = str_replace ("<!--{layout_block_{$k}}-->", $v, $content);
 			}
-			echo $content;
+
+			//是否返回
+			if($return) return $content; else echo $content;
+		}
+		else if($return)
+		{
+			ob_start();include APP_PATH . '/views/'. $file;
+			return ob_get_clean();
 		}
 		else include APP_PATH . '/views/'. $file;
     }
