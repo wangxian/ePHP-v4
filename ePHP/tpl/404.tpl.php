afe926eafe586bf2e6ecabca6b171c2e54c366b9
wangxian <xian366@126.com>
2015-07-15 09:22:26 +0800

    CHANGED: 修改404.tpl

--- ePHP/tpl/404.tpl.php
+++ ePHP/tpl/404.tpl.php
@@ -1,24 +1,18 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html>
+<html lang="zh-CN">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta charset="UTF-8" />
 <title></title>
 <style type="text/css">
-body{ font: 12px/18px "Microsoft Yahei", "Consolas",Tahoma, Geneva, sans-serif;background-color: #f2f2f2;}
-#wrapper{margin: 50px auto;width: 500px;}
-h1{
-    font-size: 6em; color: #262626; color: black;line-height:1em;
-    font-family: LeagueGothic, Haettenschweiler, "Arial Narrow Bold", sans-serif;
-    margin: 10px 0;
-    border-bottom: 3px solid #CCC;
-}
-p{ color: #666;letter-spacing: 1px;}
+body { font: 12px/18px Tahoma, Geneva, sans-serif; background-color: #f2f2f2; }
+#wrapper { margin: 50px auto; width: 500px; }
+h1 {font-size: 6em; color: #262626; line-height:1em; margin: 10px 0; border-bottom: 3px solid #CCC; }
+p { color: #666; letter-spacing: 1px;}
 </style>
 </head>
 <body>
-<div id="wrapper">
-  <h1>404 ERROR.. NOT!</h1>
-  <p>非常抱歉，没有找到您要访问的页面！</p>
-</div>
+  <div id="wrapper">
+  <h1>404 ERROR... NOT!</h1>
+    <p>非常抱歉，没有找到您要访问的页面！</p>
+  </div>
 </body>
 </html>
